8d2fe6ca1bba5445fecce536378cff3f27cb4318
Duy Le <dle@walmartlabs.com>
2018-04-26 17:03:25 -0700

    add more functiality

--- src/main/java/com/my/app/MainApp.java
+++ src/main/java/com/my/app/MainApp.java
@@ -1,5 +1,8 @@
 package com.my.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.walmart.platform.exp.client.ae.context.model.AssignmentTreatment;
 import com.walmart.platform.exp.smartcomm.client.ExpoAEClient;
 
@@ -8,9 +11,20 @@ public class MainApp {
     public static void main(String[] args) {
         try {
             ExpoAEClient client = ExpoAEClient.getInstance();
-            AssignmentTreatment treatment = client.getAssignmentTreatment("dle");
-            System.out.println("assignments: " + treatment.getAssignments());
-            System.out.println("treatments: " + treatment.getTreatments());
+            List<String> cids = Arrays.asList(
+                    "dle",
+                    "other",
+                    "expo"
+                    );
+            for (String cid : cids) {
+                System.out.println();
+                System.out.println("==============================================");
+                System.out.println("cid: " + cid);
+                System.out.println("==============================================");
+                AssignmentTreatment treatment = client.getAssignmentTreatment(cid);
+                System.out.println("assignments: " + treatment.getAssignments());
+                System.out.println("treatments: " + treatment.getTreatments());
+            }
         } catch (Throwable t) {
             t.printStackTrace();
         }
